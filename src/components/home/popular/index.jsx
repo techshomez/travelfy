95f9524a6d92a418dc4b92c3fa2c6ca549d92e94
Daniel Tope <danieltopeg@gmail.com>
2023-03-16 14:53:09 +0100

    location popular component

---
+++ src/components/home/popular/index.jsx
@@ -0,0 +1,14 @@
+import "./popular.scss";
+
+const Popular = () => {
+  return (
+    <div className="popular">
+      <div className="container">
+        <h2>Popular Package</h2>
+        <article>The most popular tour package presented to you</article>
+      </div>
+    </div>
+  );
+};
+
+export default Popular;
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,4 +1,5 @@
 import Header from "../../components/common/header";
+import Popular from "../../components/home/popular";
 import PageTitle from "../../components/reuse/pagetitle";
 
 const Home = () => {
@@ -6,18 +7,7 @@ const Home = () => {
     <>
       <PageTitle page_name="TravelFy" title="Home" />
       <Header />
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio nam ipsam
-      numquam cupiditate velit, illo commodi vitae voluptate ducimus alias
-      asperiores ex amet provident doloribus impedit neque sint laboriosam ea?
+      <Popular />
     </>
   );
 };
